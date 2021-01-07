4cb1a7b397bf10a9cb70957ceff501063bfd4fc2
Rakesh Nair <Rakesh.Nair@accelya.local>
2021-01-07 20:05:15 +0530

    Added Configuration to fetch min and max

---
+++ src/main/java/org/spring/microservices/config/Configuration.java
@@ -0,0 +1,50 @@
+package org.spring.microservices.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("limits-service")
+public class Configuration {
+	private int minimum;
+	private int maximum;
+
+	public Configuration() {
+		super();
+	}
+
+	public Configuration(int minimum, int maximum) {
+		super();
+		this.minimum = minimum;
+		this.maximum = maximum;
+	}
+
+	/**
+	 * @return the minimum
+	 */
+	public int getMinimum() {
+		return minimum;
+	}
+
+	/**
+	 * @param minimum the minimum to set
+	 */
+	public void setMinimum(int minimum) {
+		this.minimum = minimum;
+	}
+
+	/**
+	 * @return the maximum
+	 */
+	public int getMaximum() {
+		return maximum;
+	}
+
+	/**
+	 * @param maximum the maximum to set
+	 */
+	public void setMaximum(int maximum) {
+		this.maximum = maximum;
+	}
+
+}
--- src/main/java/org/spring/microservices/controller/LimitsConfigurationController.java
+++ src/main/java/org/spring/microservices/controller/LimitsConfigurationController.java
@@ -1,14 +1,19 @@
 package org.spring.microservices.controller;
 
 import org.spring.microservices.bean.LimitConfiguration;
+import org.spring.microservices.config.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LimitsConfigurationController {
 
+	@Autowired
+	private Configuration configuration;
+
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsFromConfiguratons() {
-		return new LimitConfiguration(1000, 1);
+		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
 	}
 }
